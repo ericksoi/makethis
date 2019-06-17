49c2a078edfdd2f08a7c8933d1f134cbb1dbe490
ericksoi <root@DESKTOP-FUPLPL4.localdomain>
2019-06-18 01:47:32 +0300

    .

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
 	title = models.CharField(max_length=200)
 	text = models.TextField()
 	created_date = models.DateTimeField(default=timezone.now)
-	published_date = models.DateTimeField()
+	published_date = models.DateTimeField(default=timezone.now)
 
 	def pulished(self):
 		self.published_date = timezone.now()
